10308e9839a84db32935f9c35088bb97de276318
NikitaSokolov96 <107963433+NikitaSokolov96@users.noreply.github.com>
2022-07-18 02:06:31 +0300

    Add files via upload

---
+++ seminar_6/zadacha_41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter yours massive");
+    var a = Console.ReadLine()
+        .Split(new[] {" "},StringSplitOptions.RemoveEmptyEntries)
+        .Select(Int32.Parse);
+    int positives = 0;
+    foreach (var i in a)
+{
+    if (i > 0)
+    {
+        positives++;
+    }
+}
+Console.WriteLine("Positives: {0}", positives);
